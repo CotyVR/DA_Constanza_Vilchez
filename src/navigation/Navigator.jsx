e70b94192cccbf6306a48aa34090db6e5079dd4b
Coty <cets-lukai@hotmail.com>
2024-11-17 23:08:56 -0300

    navegacion 2

--- src/navigation/Navigator.jsx
+++ src/navigation/Navigator.jsx
@@ -1,34 +1,26 @@
-import { NavigationContainer } from "@react-navigation/native"
-import { createNativeStackNavigator } from "@react-navigation/native-stack"
+import { NavigationContainer } from "@react-navigation/native";
+import { createNativeStackNavigator } from "@react-navigation/native-stack";
 
-import { CategoriesScreen, ProductsScreen, ProductScreen } from "../screens"
-import Header from "../components/Header"
+import { CategoriesScreen, ProductsScreen, ProductScreen } from "../screens";
+import Header from "../components/Header";
 
-
-const Stack = createNativeStackNavigator()
+const Stack = createNativeStackNavigator();
 
 const Navigator = () => {
-    return(
-      <NavigationContainer>
-        <Stack.Navigator
-            screenOptions = {{
-                header: ({route}) => <Header subtitle={route.name}/>
-            }}
-        >
-            <Stack.Screen name="Categorias"  component={CategoriesScreen}/>
-            
-            <Stack.Group
-                screenOptions={{
-                    color: 'blue'
-                }}
-            >
-                <Stack.Screen name="Productos"  component={ProductsScreen}/>
-                <Stack.Screen name="Producto" component={ProductScreen}/>
-            </Stack.Group>
-        </Stack.Navigator>
-      </NavigationContainer>
-    )
-}
+  return (
+    <NavigationContainer>
+      <Stack.Navigator
+        screenOptions={{
+          header: ({ route }) => <Header subtitle={route.name} />,
+        }}
+      >
+        <Stack.Screen name="Categorias" component={CategoriesScreen} />
 
-export default Navigator
+        <Stack.Screen name="Productos" component={ProductsScreen} />
+        <Stack.Screen name="Producto" component={ProductScreen} />
+      </Stack.Navigator>
+    </NavigationContainer>
+  );
+};
 
+export default Navigator;
