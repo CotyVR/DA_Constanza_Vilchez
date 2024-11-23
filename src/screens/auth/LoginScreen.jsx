8270f6c2477178b2874363d7dcbded819cf27e76
Coty <cets-lukai@hotmail.com>
2024-11-23 16:49:13 -0300

    arreglo inicio sesion

--- src/screens/auth/LoginScreen.jsx
+++ src/screens/auth/LoginScreen.jsx
@@ -1,135 +1,177 @@
-import { StyleSheet, Text, View, TextInput, Pressable, Dimensions } from 'react-native'
-import { LinearGradient } from 'expo-linear-gradient';
-import { colors } from '../../global/colors'
-import { useState, useEffect } from 'react';
-import { setUser } from '../../features/auth/authSlice';
-import { useDispatch } from 'react-redux';
-import { useLoginMutation } from '../../services/authService';
-const textInputWidth = Dimensions.get('window').width * 0.7
-const LoginScreen = ({navigation}) => {
-    const [email, setEmail] = useState("")
-    const [password, setPassword] = useState("")
-    const dispatch = useDispatch()
-    const [triggerLogin, result] = useLoginMutation()
-    useEffect(()=>{
-        if(result.status==="rejected"){
-      
-        }else if(result.status==="fulfilled"){
+import {
+  StyleSheet,
+  Text,
+  View,
+  TextInput,
+  Pressable,
+  Dimensions,
+} from "react-native";
+import { LinearGradient } from "expo-linear-gradient";
+import { colors } from "../../global/colors";
+import { useState, useEffect } from "react";
+import { setUser } from "../../features/auth/authSlice";
+import { useDispatch } from "react-redux";
+import { useLoginMutation } from "../../services/authService";
+import Icon from "react-native-vector-icons/MaterialIcons";
 
-            dispatch(setUser(result.data))
-        }
-    },[result])
-    const onsubmit = ()=>{     
-        triggerLogin({email,password})
+const textInputWidth = Dimensions.get("window").width * 0.7;
+
+const LoginScreen = ({ navigation }) => {
+  const [email, setEmail] = useState("");
+  const [password, setPassword] = useState("");
+  const [rememberMe, setRememberMe] = useState(false);
+
+  const dispatch = useDispatch();
+
+  const [triggerLogin, result] = useLoginMutation();
+  useEffect(() => {
+    if (result.status === "rejected") {
+    } else if (result.status === "fulfilled") {
+      dispatch(setUser(result.data));
     }
-    return (
-        <LinearGradient
-            colors={["#a9e159", "#5ab507"]}
-            start={{ x: 0, y: 0 }} // esquina superior izquierda
-            end={{ x: 1, y: 1 }}   // esquina inferior derecha
-            style={styles.gradient}
+  }, [result]);
+  const onsubmit = () => {
+    triggerLogin({ email, password });
+  };
+  return (
+    <LinearGradient
+      colors={["#a9e159", "#5ab507"]}
+      start={{ x: 0, y: 0 }} // esquina superior izquierda
+      end={{ x: 1, y: 1 }} // esquina inferior derecha
+      style={styles.gradient}
+    >
+      <Text style={styles.title}>Mundo Planta</Text>
+      <Text style={styles.subTitle}>Registrate</Text>
+      <View style={styles.inputContainer}>
+        <TextInput
+          onChangeText={(text) => setEmail(text)}
+          placeholderTextColor="#EBEBEB"
+          placeholder="Email"
+          style={styles.textInput}
+        />
+        <TextInput
+          onChangeText={(text) => setPassword(text)}
+          placeholderTextColor="#EBEBEB"
+          placeholder="Password"
+          style={styles.textInput}
+          secureTextEntry
+        />
+      </View>
+
+      <View style={styles.rememberMeContainer}>
+        <Text style={styles.whiteText}>Mantener sesión iniciada</Text>
+        {rememberMe ? (
+          <Pressable onPress={() => setRememberMe(!rememberMe)}>
+            <Icon name="toggle-on" size={48} color={colors.azulCobalto} />
+          </Pressable>
+        ) : (
+          <Pressable onPress={() => setRememberMe(!rememberMe)}>
+            <Icon name="toggle-off" size={48} color={colors.cafeClaro} />
+          </Pressable>
+        )}
+      </View>
+
+      <View style={styles.footTextContainer}>
+        <Text style={styles.whiteText}>¿No tienes una cuenta?</Text>
+        <Pressable onPress={() => navigation.navigate("Signup")}>
+          <Text
+            style={{
+              ...styles.whiteText,
+              ...styles.underLineText,
+            }}
+          >
+            Crea una
+          </Text>
+        </Pressable>
+      </View>
+      <Pressable style={styles.btn} onPress={onsubmit}>
+        <Text style={styles.btnText}>Iniciar sesión</Text>
+      </Pressable>
+      <View style={styles.guestOptionContainer}>
+        <Text style={styles.whiteText}>¿Solo quieres dar un vistazo?</Text>
+        <Pressable
+          onPress={() =>
+            dispatch(
+              setUser({ email: "planta@mundoplanta.com", token: "planta" })
+            )
+          }
         >
-            <Text style={styles.title}>Mundo Planta</Text>
-            <Text style={styles.subTitle}>Registrate</Text>
-            <View style={styles.inputContainer}>
-                <TextInput
-                    onChangeText={(text) => setEmail(text)}
-                    placeholderTextColor="#EBEBEB"
-                    placeholder="Email"
-                    style={styles.textInput}
-                />
-                <TextInput
-                    onChangeText={(text) => setPassword(text)}
-                    placeholderTextColor="#EBEBEB"
-                    placeholder='Password'
-                    style={styles.textInput}
-                    secureTextEntry
-                />
-            </View>
-            <View style={styles.footTextContainer}>
-                <Text style={styles.whiteText}>¿No tienes una cuenta?</Text>
-                <Pressable onPress={() => navigation.navigate('Signup')}>
-                    <Text style={
-                        {
-                            ...styles.whiteText,
-                            ...styles.underLineText
-                        }
-                    }>
-                        Crea una
-                    </Text>
-                </Pressable>
-            </View>
-            <Pressable style={styles.btn} onPress={onsubmit}><Text style={styles.btnText}>Iniciar sesión</Text></Pressable>
-            <View style={styles.guestOptionContainer}>
-                <Text style={styles.whiteText}>¿Solo quieres dar un vistazo?</Text>
-                <Pressable onPress={()=>dispatch(setUser({email:"planta@mundoplanta.com",token:"planta"}))}>
-                  <Text style={{ ...styles.whiteText, ...styles.strongText }}>Ingresa como invitado</Text>
-                </Pressable>
-            </View>
-        </LinearGradient>
-    )
-}
-export default LoginScreen
+          <Text style={{ ...styles.whiteText, ...styles.strongText }}>
+            Ingresa como invitado
+          </Text>
+        </Pressable>
+      </View>
+    </LinearGradient>
+  );
+};
+export default LoginScreen;
 const styles = StyleSheet.create({
-    gradient: {
-        flex: 1,
-        justifyContent: 'center',
-        alignItems: 'center'
-    },
-    title: {
-        color: colors.crema,
-        fontFamily: "Satisfy",
-        fontSize: 40,
-    },
-    subTitle: {
-        fontFamily: "Inter",
-        fontSize: 18,
-        color: colors.cafeClaro,
-        fontWeight: "700",
-        letterSpacing: 3,
-    },
-    inputContainer: {
-        gap: 16,
-        margin: 16,
-        marginTop: 48,
-        alignItems: 'center',
-    },
-    textInput: {
-        padding: 8,
-        paddingLeft: 16,
-        borderRadius: 16,
-        backgroundColor: "#95859E",
-        width: textInputWidth,
-        color: colors.blanco,
-    },
-    footTextContainer: {
-        flexDirection: 'row',
-        gap: 8,
-    },
-    whiteText: {
-        color: colors.blanco
-    },
-    underLineText: {
-        textDecorationLine: 'underline',
-    },
-    strongText: {
-        fontWeight: '900',
-        fontSize: 16
-    },
-    btn: {
-        padding: 16,
-        paddingHorizontal: 32,
-        backgroundColor: colors.azulCobalto,
-        borderRadius: 16,
-        marginTop: 32,
-    },
-    btnText: {
-        color: colors.blanco,
-        fontSize: 16,
-        fontWeight: '700'
-    },
-    guestOptionContainer: {
-        alignItems: 'center',
-        marginTop: 64
-    }
-})
+  gradient: {
+    flex: 1,
+    justifyContent: "center",
+    alignItems: "center",
+  },
+  title: {
+    color: colors.crema,
+    fontFamily: "Satisfy",
+    fontSize: 40,
+  },
+  subTitle: {
+    fontFamily: "Inter",
+    fontSize: 18,
+    color: colors.cafeClaro,
+    fontWeight: "700",
+    letterSpacing: 3,
+  },
+  inputContainer: {
+    gap: 16,
+    margin: 16,
+    marginTop: 48,
+    alignItems: "center",
+  },
+  textInput: {
+    padding: 8,
+    paddingLeft: 16,
+    borderRadius: 16,
+    backgroundColor: "#95859E",
+    width: textInputWidth,
+    color: colors.blanco,
+  },
+  footTextContainer: {
+    flexDirection: "row",
+    gap: 8,
+  },
+  whiteText: {
+    color: colors.blanco,
+  },
+  underLineText: {
+    textDecorationLine: "underline",
+  },
+  strongText: {
+    fontWeight: "900",
+    fontSize: 16,
+  },
+  btn: {
+    padding: 16,
+    paddingHorizontal: 32,
+    backgroundColor: colors.azulCobalto,
+    borderRadius: 16,
+    marginTop: 32,
+  },
+  btnText: {
+    color: colors.blanco,
+    fontSize: 16,
+    fontWeight: "700",
+  },
+  guestOptionContainer: {
+    alignItems: "center",
+    marginTop: 64,
+  },
+  rememberMeContainer: {
+    flexDirection: "row",
+    gap: 5,
+    justifyContent: "space-around",
+    alignItems: "center",
+    marginVertical: 8,
+}
+});
